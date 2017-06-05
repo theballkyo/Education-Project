af4ff8d2cae5ea9120736bffd50038ee2a838afd
Krissada Ingarn <theball_kyo@hotmail.com>
2017-06-05 14:01:53 -0700

    Remove not used code

--- client/src/components/layouts/MainV1.vue
+++ client/src/components/layouts/MainV1.vue
@@ -11,7 +11,7 @@
 <script>
 import NavBar from '../NavBar.vue'
 import FooterBox from '../FooterBox.vue'
-import api from '@/api/'
+// import api from '@/api/'
 
 export default {
   name: 'layout-mainv1',
@@ -30,40 +30,40 @@ export default {
       }
     }
   },
-  methods: {
-    async fetchCourse (filters = {}) {
-      let courses = []
-      try {
-        const courses_ = await api.course.getCourses({filters})
-        courses = courses_.body
-      } catch (e) {
+  // methods: {
+  //   async fetchCourse (filters = {}) {
+  //     let courses = []
+  //     try {
+  //       const courses_ = await api.course.getCourses({filters})
+  //       courses = courses_.body
+  //     } catch (e) {
 
-      }
-      return courses
-    },
-    async subjectChange ({subject, keys}) {
-      this.course[keys].isLoading = true
-      const courses = await this.fetchCourse({subject})
-      this.course[keys].data = courses
-      this.course[keys].isLoading = false
-    }
-  },
-  watch: {
-    async '$route' () {
-      const courses = await this.fetchCourse()
-      this.course.newer.data = courses
-      this.course.rating.data = courses
-      this.course.newer.isLoading = false
-      this.course.rating.isLoading = false
-    }
-  },
-  async mounted () {
-    const courses = await this.fetchCourse()
-    this.course.newer.data = courses
-    this.course.rating.data = courses
-    this.course.newer.isLoading = false
-    this.course.rating.isLoading = false
-  },
+  //     }
+  //     return courses
+  //   },
+  //   async subjectChange ({subject, keys}) {
+  //     this.course[keys].isLoading = true
+  //     const courses = await this.fetchCourse({subject})
+  //     this.course[keys].data = courses
+  //     this.course[keys].isLoading = false
+  //   }
+  // },
+  // watch: {
+  //   async '$route' () {
+  //     const courses = await this.fetchCourse()
+  //     this.course.newer.data = courses
+  //     this.course.rating.data = courses
+  //     this.course.newer.isLoading = false
+  //     this.course.rating.isLoading = false
+  //   }
+  // },
+  // async mounted () {
+  //   const courses = await this.fetchCourse()
+  //   this.course.newer.data = courses
+  //   this.course.rating.data = courses
+  //   this.course.newer.isLoading = false
+  //   this.course.rating.isLoading = false
+  // },
   components: {
     NavBar,
     FooterBox
